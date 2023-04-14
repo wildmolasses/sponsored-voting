a0cb65ae5ea23439837003489f10c876581c093d
wildmolasses <changes@gmail.com>
2023-04-14 20:33:42 +0900

    add OZ

---
+++ src/GovernorSponsoredVoting.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import {Governor} from "@openzeppelin/contracts/governance/Governor.sol";
+
+abstract contract GovernorSponsoredVoting is Governor {}
